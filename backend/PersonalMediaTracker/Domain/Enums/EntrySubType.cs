3d7c920c341266cf77818c373b2736188944c529
Steven Brady <bradys@my.lanecc.edu>
2025-09-26 20:40:46 -0700

    adding enum classes EntryStatus, EntrySubType, and EntryType

---
+++ backend/PersonalMediaTracker/Domain/Enums/EntrySubType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum EntrySubType
+    {
+        LiveAction = 0,
+        Anime = 1,
+        Manga = 2,
+        Animated = 3,
+        Documentary = 4,
+        Other = 5
+    }
+}
